d27ebda0040154f7af7dd817593bd4ff4f182938
g2384 <g2384@users.noreply.github.com>
2022-03-29 10:54:10 +0100

    highlight results

---
+++ src/Data.cs
@@ -0,0 +1,13 @@
+﻿namespace TextExtractor
+{
+    public class Data
+    {
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public string FullPath { get; set; }
+        public string Directory { get; set; }
+        public string ModifiedTime { get; set; }
+        public string[] Paragraphs { get; set; }
+    }
+}
+
--- src/Program.cs
+++ src/Program.cs
@@ -7,15 +7,6 @@ using Toxy;
 
 namespace TextExtractor
 {
-    public class Data
-    {
-        public string FileName { get; set; }
-        public string Extension { get; set; }
-        public string FullPath { get; set; }
-        public string Directory { get; set; }
-        public string ModifiedTime { get; set; }
-        public string[] Paragraphs { get; set; }
-    }
     public static class Program
     {
         public static void Main(params string[] paths)
